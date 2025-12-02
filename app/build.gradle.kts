613d57a5d8d09a58f71fac0360018ebe6e58bdf5
nikjoseph6794 <nikkyjoseph000@gmail.com>
2025-12-02 15:13:15 +0530

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.bookshlef.bookshelf"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2"
     }
 
     buildFeatures {
